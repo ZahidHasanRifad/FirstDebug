965868e3f92891ea610ac7a0ce6964fe59554a77
Rifad <zrifad815@gmail.com>
2018-01-29 09:44:47 +0600

    first commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+
+    int x =5,z;
+    for(int y=10; y>=0; y--)
+    {
+        z = 50/(y-x);
+    }
+
+    cout << "debug complete?" << endl;
+    return 0;
+}
